13d4da305da70faeb3100996d402c3bf6bedf27c
Haitham Gad <haitham@rehashly.com>
2022-04-29 05:36:23 -0700

    Center ButtonLink text

--- components/ButtonLink.tsx
+++ components/ButtonLink.tsx
@@ -8,7 +8,7 @@ export function ButtonLink(props: ButtonLinkProps) {
   return (
     <Link
       className={
-        'rounded-md bg-primary-500 py-2 px-4 font-medium text-white focus:outline-none focus:ring-2 focus:ring-primary-600 ' +
+        'rounded-md bg-primary-500 py-2 px-4 text-center font-medium text-white focus:outline-none focus:ring-2 focus:ring-primary-600 ' +
         `hover:bg-primary-700 focus:ring-offset-2 dark:ring-offset-black dark:hover:bg-primary-400 ${className}`
       }
       {...rest}
