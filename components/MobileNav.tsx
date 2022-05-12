cadd5fb93245e0a33a9b018755f11017277dab38
Haitham Gad <haitham@rehashly.com>
2022-05-12 05:50:47 -0700

    Improve MobileNav styling

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -11,7 +11,7 @@ export function MobileNav() {
         document.body.style.overflow = 'auto'
       } else {
         // Prevent scrolling
-        document.body.style.overflow = 'hidden'
+        //document.body.style.overflow = 'hidden'
       }
       return !status
     })
@@ -31,26 +31,37 @@ export function MobileNav() {
           fill="currentColor"
           className="text-gray-900 dark:text-gray-100"
         >
-          {navShow ? (
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={`fixed top-0 right-0 z-10 h-full w-2/3 transform border-gray-300 bg-white shadow-2xl shadow-lg duration-300 ease-in-out dark:bg-gray-800 ${
+          navShow ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <button
+          type="button"
+          className="fixed top-10 right-4 z-20 ml-1 mr-1 h-8 w-8 rounded py-1"
+          aria-label="Toggle Menu"
+          onClick={onToggleNav}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="text-gray-900 dark:text-gray-100"
+          >
             <path
               fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
               clipRule="evenodd"
             />
-          ) : (
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          )}
-        </svg>
-      </button>
-      <div
-        className={`fixed top-24 right-0 z-10 h-full w-full transform bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-gray-800 ${
-          navShow ? 'translate-x-0' : 'translate-x-full'
-        }`}
-      >
+          </svg>
+        </button>
         <button
           type="button"
           aria-label="toggle modal"
@@ -62,7 +73,7 @@ export function MobileNav() {
             <div key={link.title} className="px-12 py-4">
               <Link
                 href={link.href}
-                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                className="text-xl font-extrabold text-gray-900 dark:text-gray-100"
                 onClick={onToggleNav}
               >
                 {link.title}
